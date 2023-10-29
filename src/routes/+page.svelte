56fa251950ec94af00b53a6d2289c2bad777f265
TomerJ <totolavanda@gmail.com>
2023-10-29 14:50:03 +1000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import "../app.css";
+</script>
+
+  <svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+  </svelte:head>
+
+
+  <div class="w-screen h-screen bg-gray-900 flex items-center">
+    <div class="text-left text-white font-roboto font-light pl-16">
+      <h1 class="text-6xl mb-2.5">Hi, I'm monkey_business</h1>
+      <h1 class="text-2xl mb-5">I'm a full-stack developer. I like creating random projects and stuff</h1>
+    </div>
+	
+  </div>
