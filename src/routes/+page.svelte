7e9d4c8ca2b3b2656a45180035a6cedcfe9923ef
TomerJ <totolavanda@gmail.com>
2023-10-30 21:17:53 +1000

    skills page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
   <div class="w-screen h-screen bg-gray-900 flex items-center">
     <div class="text-left text-white font-roboto font-light pl-16">
-      <h1 class="text-6xl mb-2.5">Hi, I'm monkey_business</h1>
+      <h1 class="text-6xl mb-2.5 font-bold">Hi, I'm monkey_business</h1>
       <h1 class="text-2xl mb-2">I'm a full-stack developer. I like creating random projects and stuff</h1>
       <hr class="mb-2">
       <div class="flex text-sm">
---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+	import "../../app.css";
+
+  import { Svelte, Tailwindcss, Css3, Javascript, Html5, Typescript, Axios, Express, Nodedotjs, Mongodb, Redis, Adobeillustrator, Adobephotoshop, Adobepremierepro, Robloxstudio, Trello, Lua, Visualstudiocode, Intellijidea, Discord, Linux, Ubuntu, Vite, Nginx, Cloudflare } from "@inqling/svelte-icons/simple-icons";
+</script>
+
+  <svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+  </svelte:head>
+
+
+  <div class="w-screen h-screen bg-gray-900 flex items-center">
+    <div class="text-left text-white font-roboto font-light pl-16">
+      <div>
+        <h1 class="text-6xl mb-2.5">Skills</h1>
+        
+
+        <hr class="mb-5">
+      </div>
+      <div class="text-md mb-4">
+        <p class="font-bold">Frontend</p>
+        <div class="flex">
+          <Svelte class="icon w-8 mr-1" />
+          <Tailwindcss class="icon w-8 mr-1" />
+          <Css3 class="icon w-8 mr-1" />
+          <Javascript class="icon w-8 mr-1" />
+          <Html5 class="icon w-8 mr-1" />
+        </div>
+      </div>
+      <div class="text-md mb-4">
+        <p class="font-bold">Backend</p>
+        <div class="flex">
+          <Typescript class="icon w-8 mr-1" />
+          <Axios class="icon w-8 mr-1" />
+          <Express class="icon w-8 mr-1" />
+          <Nodedotjs class="icon w-8 mr-1" />
+        </div>
+      </div>
+      <div class="text-md mb-4">
+        <p class="font-bold">Database</p>
+        <div class="flex">
+          <Redis class="icon w-8 mr-1" />
+          <Mongodb class="icon w-8 mr-1" />
+        </div>
+      </div>
+      <div class="text-md mb-4">
+        <p class="font-bold">Graphic Design & Video Editing</p>
+        <div class="flex">
+          <Adobephotoshop class="icon w-8 mr-1" />
+          <Adobeillustrator class="icon w-8 mr-1" />
+          <Adobepremierepro class="icon w-8 mr-1" />
+        </div>
+      </div>
+      <div class="text-md mb-4">
+        <p class="font-bold">Tools & Other Languages</p>
+        <div class="flex">
+          <Robloxstudio class="icon w-8 mr-1" />
+          <Trello class="icon w-8 mr-1" />
+          <Lua class="icon w-8 mr-1" />
+          <Visualstudiocode class="icon w-8 mr-1" />
+          <Intellijidea class="icon w-8 mr-1" />
+          <Discord class="icon w-8 mr-1" />
+          <Linux class="icon w-8 mr-1" />
+          <Ubuntu class="icon w-8 mr-1" />
+          <Vite class="icon w-8 mr-1" />
+          <Nginx class="icon w-8 mr-1" />
+          <Cloudflare class="icon w-8 mr-1" />
+        </div>
+      </div>
+
+    </div>
+	
+  </div>
